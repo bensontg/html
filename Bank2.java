b55048da447d1d3f521fdbcff87c7a4495729137
bensontg <benzontg@gmail.com>
2021-09-14 13:41:43 +0530

    sample

---
+++ Bank2.java
@@ -0,0 +1,16 @@
+class Bank2
+{
+	static int no,no1,no2,i;
+	Bank2(no,no1)
+	{
+		no2=no+no1
+		return no2
+	}
+	public static void main(String[] arg)
+	{
+		Bank2.Bank2(5,55)
+		i=Bank2.no2;
+		System.out.print(i);
+		
+	}
+}
